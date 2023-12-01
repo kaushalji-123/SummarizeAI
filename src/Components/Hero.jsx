eaa018a7f9340adff1d50ab40cbfe188d01fa9dd
kaushalji-123 <divyanshu838283fb@gmail.com>
2023-12-01 13:17:17 +0530

    fixing p tag with logo

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,9 +3,11 @@ import {logo} from '../assets';
 const hero = () => {
   return (
     <header className='w-full flx justify-center items-center flex-col'>
-        <nav className="flex justify-between mt-7 mb-7 ">
-            <img src="./src/assets/logo.svg" alt="sumz_logo"
+        <nav className="flex justify-between mb-7 ">
+            <div className="logo">
+            <img src="./src/assets/summry.ai.png" alt="sumz_logo"
              className="w-28 object-contain"/>
+             </div>
              <button className="nav_button"> Github </button>
         </nav>
             <h1 className="head_text"> Summarize Artices with
@@ -13,7 +15,7 @@ const hero = () => {
             <span
              className="orange_gradient">OpenAi GPT-4</span>
              </h1>
-             <h2 className="desc">
+             <h2 className="desc ">
              Effortlessly summarize any web or article link with this intelligent tool. It generates concise, meaningful summaries, providing a quick reference to key points within articles, ultimately saving valuable time.
              </h2>
     </header>
