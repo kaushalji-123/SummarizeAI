ae9ddbeb040462df61c11ee326dd013fb2f47728
kaushalji-123 <divyanshu838283fb@gmail.com>
2023-12-19 01:35:01 +0530

    fix logo issue

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import {logo} from '../assets';
 const hero = () => {
   return (
-    <header className='w-full flx justify-center items-center flex-col'>
-        <nav className="flex justify-between mb-7 ">
+    <header className='w-full flex justify-center items-center flex-col'>
+        <nav className="flex justify-between mb-7 bg">
             <div className="logo ">
             <img src="./src/assets/summlogo.png" alt="sumz_logo"
-             className="w-28 object-contain"/>
+             className="w-28"/>
              </div>
              <button className="nav_button" href="https://www.linkedin.com/in/divyanshu-kaushal-73a428209/"> Github </button>
         </nav>
@@ -15,7 +14,7 @@ const hero = () => {
             <span
              className="orange_gradient">OpenAi GPT-4 </span>
              </h1>
-             <h2 className="desc ">
+             <h2 className="desc">
              Effortlessly summarize any web or article link with this intelligent tool. It generates concise, meaningful summaries, providing a quick reference to key points within articles, ultimately saving valuable time.
              </h2>
     </header>
